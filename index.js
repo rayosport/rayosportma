56c9abb8e9d34e00a939911ca2534aaf15fd6c42
mohamedaittaleb <mohamedaittaleb1994@gmail.com>
2025-04-29 23:21:29 +0100

    Updates

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 var vite_config_default = defineConfig({
+  base: "/rayosportma/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
